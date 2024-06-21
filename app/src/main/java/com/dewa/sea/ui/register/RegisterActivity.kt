c6c11542453f308b8d7fbf1519805b5462c4e290
dewatriwijaya <dewatwc@gmail.com>
2024-06-21 11:16:07 +0700

    update RegisterActivity

---
+++ app/src/main/java/com/dewa/sea/ui/register/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.dewa.sea.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dewa.sea.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
